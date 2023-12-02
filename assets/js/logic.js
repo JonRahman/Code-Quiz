3efd9118f79129e854e2e61bd5b38d66427b1a00
JonRahman <jonrahman90@gmail.com>
2023-12-02 20:48:16 +0000

    added questions template and var options in logic

---
+++ assets/js/logic.js
@@ -0,0 +1,21 @@
+
+let timer = document.querySelector("#timer")
+let startButton = document.querySelector("#start")
+let question = document.querySelector("#questions")
+let choice = document.querySelector("#choice")
+let feedback = document.querySelector("#feedback")
+let initial = document.querySelector("#initials")
+let submitButton = document.querySelector("#submit")
+
+function startQuiz() {
+}
+function question() {
+}
+function checkAnswer() {
+}
+function timer() {
+}
+function updateTimer() {
+}
+function endQuiz() {
+}
---
+++ assets/js/questions.js
@@ -0,0 +1,13 @@
+const questions = [
+    {
+      title: "Question 1?",
+      choices: ["Choice A", "Choice B", "Choice C", "Choice D"],
+      correctIndex: 0
+    },
+    {
+      title: "Question 2?",
+      choices: ["Choice A", "Choice B", "Choice C", "Choice D"],
+      correctIndex: 1
+    },
+  ];
+  
