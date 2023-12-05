8335775b4ac2622399649afe86f4fe66eb731aab
JonRahman <jonrahman90@gmail.com>
2023-12-05 00:38:22 +0000

    when wrong question is selected the timer minus 25seconds

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,7 +7,7 @@ let feedback = document.querySelector("#feedback")
 let initial = document.querySelector("#initials")
 let submitButton = document.querySelector("#submit")
 
-let time = 100;
+let time = 50;
 let timeCount;
 
 function setupTimer() {
@@ -37,13 +37,13 @@ function setupTimer() {
 
 let currentQuestionIndex = 0;
 
-document.getElementById('start-screen').addEventListener('click', function() {
-    document.getElementById('start-screen').setAttribute('class','hide');
+document.getElementById('start-screen').addEventListener('click', function () {
+    document.getElementById('start-screen').setAttribute('class', 'hide');
     document.getElementById('questions').removeAttribute('class');
     showQuestion();
     setupTimer();
 });
-  
+
 function showQuestion() {
     const currentQuestion = questions[currentQuestionIndex];
     document.getElementById('question-title').textContent = currentQuestion.title;
@@ -53,43 +53,48 @@ function showQuestion() {
     document.getElementById('choices').innerHTML = '';
 
     // Create new buttons for each question choice
-    currentQuestion.choices.forEach(function(choice, index) {
+    currentQuestion.choices.forEach(function (choice, index) {
         const button = document.createElement('button');
         button.textContent = choice;
-        button.setAttribute('value',choice)
+        button.setAttribute('value', choice)
         choices.appendChild(button);
         // choices.addEventListener('click', function(event) {
         //     console.log(event.target.value)
-            
-     //   });
-        
+
+        //   });
+
     });
 }
 
-    function buttonClick(event) {
-        var targetEl = event.target;
-        console.log(targetEl.value)
-        if (targetEl.value === questions[currentQuestionIndex].correctIndex) {
-            console.log('Correct!');
-        } else {
-            console.log('Incorrect.');
+function buttonClick(event) {
+    var targetEl = event.target;
+    console.log(targetEl.value)
+    if (targetEl.value === questions[currentQuestionIndex].correctIndex) {
+        console.log('Correct!');
+    } else {
+        console.log('Incorrect.');
+        time -= 25; // Decrease the time by 25 seconds
+        if (time < 0) {
+            time = 0; // Make sure the time doesn't go below 0
         }
-        currentQuestionIndex++
-        if (currentQuestionIndex === questions.length){
-            gameOver()
+        timer.textContent = "Time: " + time; // Update the timer element's text content
+    }
+    currentQuestionIndex++
+    if (currentQuestionIndex === questions.length) {
+        gameOver()
     }
     else {
         showQuestion();
-    }}
-
-    function gameOver() {
-        document.getElementById('questions').setAttribute('class','hide');
-        document.getElementById('end-screen').removeAttribute('class');
     }
+}
 
-    choices.onclick=buttonClick 
+function gameOver() {
+    document.getElementById('questions').setAttribute('class', 'hide');
+    document.getElementById('end-screen').removeAttribute('class');
+}
 
-    // function endQuiz() {
-    //     // Your logic for ending the quiz
-    // }
-    
+choices.onclick = buttonClick
+
+function endQuiz() {
+    // Your logic for ending the quiz
+}
